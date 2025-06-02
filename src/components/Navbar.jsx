80e1313ba953c8387de99b7ecd068e3601659bc2
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-02 23:50:11 +0530

    added outfit google font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="bg-[#333] text-white flex justify-between items-center px-10">
+        <nav className="bg-[#333] text-white font-outfit flex justify-between items-center px-10">
             <a href="/" className="text-inherit">REJAP</a>
             <ul className="flex justify-between items-center gap-12">
                 <li><a href="/chapters">CHAPTERS</a></li>
