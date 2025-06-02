33d1ea04bf17a23955eca861de068673566a1f8f
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-03 00:09:34 +0530

    added navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
     return (
-        <nav className="bg-[#333] text-white font-outfit flex justify-between items-center px-10">
-            <a href="/" className="text-inherit">REJAP</a>
-            <ul className="flex justify-between items-center gap-12">
+        <nav className="text-[#F3F3EE] font-outfit flex justify-between items-center px-10 py-4 fixed top-0 left-0 w-full z-99 shadow-md">
+            <a href="/" className="text-inherit text-4xl font-semibold">REJAP</a>
+            <ul className="text-xl font-semibold flex justify-between items-center gap-12">
                 <li><a href="/chapters">CHAPTERS</a></li>
                 <li><a href="/resources">RESOURCES</a></li>
                 <li><a href="/log-in">SIGN IN</a></li>
