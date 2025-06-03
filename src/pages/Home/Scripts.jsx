a591417937720e3f6968181a96e0cdbf2eb7c33d
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-03 18:41:01 +0530

    adjusted scripts section styling

--- src/pages/Home/Scripts.jsx
+++ src/pages/Home/Scripts.jsx
@@ -2,8 +2,8 @@ export default function Scripts(){
     return (
         <>
             <section id="scriptsSection" className="h-screen flex justify-center">
-                <div className="h-[90%] w-[70%] mt-22 flex flex-col">
-                    <div className="w-full h-[92%] flex flex-col">
+                <div className="h-[90%] w-[70%] mt-15 flex flex-col">
+                    <div className="w-full h-[90%] flex flex-col">
                         <div className="scriptsHeading w-full h-[15%] flex flex-col justify-center items-center">
                             <p className="font-outfit font-regular text-4xl text-amber-50">Japanese language uses</p>
                             <p className="font-outfit font-bold text-6xl text-amber-50">THREE SCRIPTS</p>
@@ -26,11 +26,13 @@ export default function Scripts(){
                             </div>
                         </div>
                     </div>
-                    <div className="w-full h-[8%]">
-                        <a href="#chaptersSection" className="flex flex-col justify-center items-center animate-float mt-3">
-                            <p className="font-bold text-amber-50 text-2xl mt-2">SCROLL DOWN</p>
-                            <svg className="-translate-y-3" xmlns="http://www.w3.org/2000/svg" width="48px" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z" stroke="#e3e3e3" stroke-width="30"/></svg>
-                        </a>
+                    <div className="w-full h-[10%]">
+                        <div className="flex flex-col justify-center items-center animate-float mt-12">
+                            <a href="#chaptersSection" className="text-center flex justify-center items-center flex-col">
+                                <p className="font-bold text-amber-50 text-2xl mt-2">SCROLL DOWN</p>
+                                <svg className="-translate-y-3" xmlns="http://www.w3.org/2000/svg" width="48px" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z" stroke="#e3e3e3" stroke-width="30"/></svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
