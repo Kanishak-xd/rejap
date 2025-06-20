07ef96beb93210306e3b345d0fa67a644b84881d
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-21 06:01:36 +0900

    completed leftFrame of sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import LeftFrame from './leftFrame/leftFrame'
+import RightFrame from './rightFrame/rightFrame'
 
 export default function SignIn() {
     return (
-        <div className='bg-amber-300 w-full h-full mt-[4%]'>
-
+        <div className='w-full h-dvh flex flex-row justify-center items-center'>
+            <LeftFrame />
+            <RightFrame />
         </div>
     )
 }
---
+++ src/pages/SignIn/rightFrame/rightFrame.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RightFrame() {
+    return (
+        <div className='w-[30%] h-[88%] mt-12 border-2 border-white border-fade rounded-xl'></div>
+    )
+}
