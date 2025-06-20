00a519c44f9a3d5b2fc1d5bca7c4eeb9f9f6d581
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-06-21 04:50:14 +0900

    changed app bg-color and added empty sign in page directories and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './pages/Home/Home'
+// import Home from './pages/Home/Home'
+import SignIn from './pages/SignIn/SignIn'
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <SignIn />
     </>
   )
 }
---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignIn() {
+    return (
+        <div className='bg-amber-300 w-full h-full mt-[4%]'>
+
+        </div>
+    )
+}
