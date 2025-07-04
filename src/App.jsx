31655edde926ba6ed705d790305b5899ebdcd997
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-05 02:13:34 +0530

    added protected routes and implemented route guard

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './pages/Home/Home'
-import SignIn from './pages/SignIn/SignIn'
-import Levels from './pages/Levels/Levels'
-import LevelIntro from './pages/LevelIntro/LevelIntro'
-import QuizPage from './pages/Quiz/QuizPage'
-import Profile from './pages/Profile/Profile'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
+import SignIn from './pages/SignIn/SignIn';
+import Levels from './pages/Levels/Levels';
+import LevelIntro from './pages/LevelIntro/LevelIntro';
+import QuizPage from './pages/Quiz/QuizPage';
+import Profile from './pages/Profile/Profile';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './context/ProtectedRoute';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/log-in" element={<Navigate to="/sign-in" />} />
-        <Route path="/sign-up" element={<Navigate to="/sign-in" />} />
-        <Route path="/levels" element={<Levels />} />
-        <Route path="/levels/:chapterId/:levelId" element={<LevelIntro />} />
-        <Route path="/levels/:chapter/:level/quiz" element={<QuizPage />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/log-in" element={<Navigate to="/sign-in" />} />
+          <Route path="/sign-up" element={<Navigate to="/sign-in" />} />
+          <Route path="/levels" element={<Levels />} />
+
+          {/* Protected Routes */}
+          <Route path="/levels/:chapterId/:levelId" element={<ProtectedRoute> <LevelIntro /> </ProtectedRoute>} />
+          <Route path="/levels/:chapter/:level/quiz" element={<ProtectedRoute> <QuizPage /> </ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute> <Profile /> </ProtectedRoute>} />
+
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
