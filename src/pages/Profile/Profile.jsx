9bf8b9d5990fafdf256b5909db70efec59876cbc
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-07 04:19:58 +0530

    added progress cards and updated styling of profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -129,73 +129,144 @@ export default function Profile() {
     };
 
     return (
-        <div className="min-h-screen bg-[#121212] text-white font-outfit flex flex-col items-center pt-24">
-            <ProfilePicture
-                selectedFile={selectedFile}
-                setSelectedFile={setSelectedFile}
-                setPreviewUrl={setPreviewUrl}
-                previewUrl={previewUrl || profilePic}
-            />
-
-            <div className="text-center mt-6">
-                <h2 className="text-3xl font-semibold">{username || "Unnamed"}</h2>
-                <p className="text-gray-400">{email || "No email"}</p>
-            </div>
-
-            <button
-                className="mt-6 bg-blue-600 px-6 py-2 rounded hover:bg-blue-700 transition"
-                onClick={() => setShowResetPopup(true)}
-            >
-                Change Password
-            </button>
-
-            <button
-                onClick={handleLogout}
-                className="mt-3 text-red-500 hover:underline"
-            >
-                Log Out
-            </button>
-
-            {selectedFile && (
-                <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-green-600 px-6 py-3 rounded shadow-lg text-white z-50 animate-toast-slide-in">
-                    <button onClick={handleSave}>Save Changes</button>
-                </div>
-            )}
-
-            {showResetPopup && (
-                <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
-                    <div className="bg-[#1e1e1e] p-6 rounded-lg w-[90%] max-w-sm text-white shadow-xl">
-                        <h3 className="text-xl font-semibold mb-4">Reset Password</h3>
-                        <input
-                            type="email"
-                            value={resetEmail}
-                            onChange={(e) => setResetEmail(e.target.value)}
-                            placeholder="Enter your email"
-                            className="w-full mb-3 px-4 py-2 rounded bg-[#2a2a2a] text-white focus:outline-none"
-                        />
-                        <div className="flex justify-between">
-                            <button
-                                className="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700 transition"
-                                onClick={handleReset}
-                            >
-                                Send Link
-                            </button>
-                            <button
-                                className="text-red-400 hover:underline"
-                                onClick={() => {
-                                    setShowResetPopup(false);
-                                    setStatus('');
-                                }}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                        {status && (
-                            <p className="mt-3 text-sm text-green-400">{status}</p>
-                        )}
+        <div className="min-h-screen bg-black text-white font-outfit flex flex-col items-center pt-24">
+            <div className='w-[45%] flex flex-col'>
+                <p className='font-bold text-5xl pb-7'>PROFILE</p>
+                <div className='flex bg-[#171717] items-center w-full h-49 rounded-lg'>
+                    <ProfilePicture
+                        selectedFile={selectedFile}
+                        setSelectedFile={setSelectedFile}
+                        setPreviewUrl={setPreviewUrl}
+                        previewUrl={previewUrl || profilePic}
+                    />
+                    <div className="text-left flex flex-col gap-3 pl-5">
+                        <p className="text-neutral-200 text-5xl font-semibold">{username || "Unnamed"}</p>
+                        <p className="text-neutral-300 text-4xl font-medium">Joined Sep 09, 1996</p>
+                        <p className="text-neutral-400 text-2xl">{email || "No email"}</p>
                     </div>
                 </div>
-            )}
+                <div className="divider w-full pt-10 pb-9"></div>
+                <p className='font-bold text-5xl pb-7'>PROGRESS</p>
+                <div className="grid grid-cols-2 grid-rows-2 gap-7 w-full h-72">
+                    <div className="bg-neutral-900 rounded-lg flex flex-row gap-2">
+                        <div className="basis-1/3 flex justify-center items-center">
+                            <div className='w-3/4 h-3/4 flex justify-center items-center rounded-md bg-[#BFECFF]'>
+                                <img src='https://res.cloudinary.com/dykzzd9sy/image/upload/v1751840508/torii_sbc71f.webp' className='bg-cover w-2/3 h-2/3' />
+                            </div>
+                        </div>
+                        <div className="basis-2/3 flex flex-col justify-center">
+                            <p className='font-semibold text-xl'>Chapter 1: Hiragana</p>
+                            <p className='font-regular text-lg'>Levels completed: 3</p>
+                            <div className="w-5/7 mt-2 bg-gray-200 rounded-xs dark:bg-black flex justify-start items-center h-3">
+                                <div className="bg-[#BFECFF] h-[70%] p-0.5 leading-none w-[45%] mx-0.5 rounded-xs"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-neutral-900 rounded-lg flex flex-row gap-2">
+                        <div className="basis-1/3 flex justify-center items-center">
+                            <div className='w-3/4 h-3/4 flex justify-center items-center rounded-md bg-[#CDC1FF]'>
+                                <img src='https://res.cloudinary.com/dykzzd9sy/image/upload/v1751840508/sushi_ss7txh.webp' className='bg-cover w-2/3 h-2/3' />
+                            </div>
+                        </div>
+                        <div className="basis-2/3 flex flex-col justify-center">
+                            <p className='font-semibold text-xl'>Chapter 2: Katakana</p>
+                            <p className='font-regular text-lg'>Levels completed: 3</p>
+                            <div className="w-5/7 mt-2 bg-gray-200 rounded-xs dark:bg-black flex justify-start items-center h-3">
+                                <div className="bg-[#CDC1FF] h-[70%] p-0.5 leading-none w-[45%] mx-0.5 rounded-xs"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-neutral-900 rounded-lg flex flex-row gap-2">
+                        <div className="basis-1/3 flex justify-center items-center">
+                            <div className='w-3/4 h-3/4 flex justify-center items-center rounded-md bg-[#FFF6E3]'>
+                                <img src='https://res.cloudinary.com/dykzzd9sy/image/upload/v1751840508/sensu_iusuf7.webp' className='bg-cover w-2/3 h-2/3' />
+                            </div>
+                        </div>
+                        <div className="basis-2/3 flex flex-col justify-center">
+                            <p className='font-semibold text-xl'>Chapter 3: Kanji</p>
+                            <p className='font-regular text-lg'>Levels completed: 3</p>
+                            <div className="w-5/7 mt-2 bg-gray-200 rounded-xs dark:bg-black flex justify-start items-center h-3">
+                                <div className="bg-[#FFF6E3] h-[70%] p-0.5 leading-none w-[45%] mx-0.5 rounded-xs"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-neutral-900 rounded-lg flex flex-row gap-2">
+                        <div className="basis-1/3 flex justify-center items-center">
+                            <div className='w-3/4 h-3/4 flex justify-center items-center rounded-md bg-[#FFCCEA]'>
+                                <img src='https://res.cloudinary.com/dykzzd9sy/image/upload/v1751840508/maneki-neko_cugqw9.webp' className='bg-cover w-2/3 h-2/3' />
+                            </div>
+                        </div>
+                        <div className="basis-2/3 flex flex-col justify-center">
+                            <p className='font-semibold text-xl'>Chapter 4: Time</p>
+                            <p className='font-regular text-lg'>Levels completed: 3</p>
+                            <div className="w-5/7 mt-2 bg-gray-200 rounded-xs dark:bg-black flex justify-start items-center h-3">
+                                <div className="bg-[#FFCCEA] h-[70%] p-0.5 leading-none w-[45%] mx-0.5 rounded-xs"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="divider w-full pt-10 pb-9"></div>
+                <p className='font-bold text-5xl pb-7'>ACCOUNT</p>
+                <button
+                    className="flex flex-row gap-2 w-48 text-neutral-500 text-xl text-left hover:text-neutral-300 cursor-pointer transition"
+                    onClick={() => setShowResetPopup(true)}
+                >
+                    Change password
+                    <svg class="w-5 h-5 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
+                    </svg>
+                </button>
+                <button
+                    onClick={handleLogout}
+                    className="mt-3 pb-16 flex flex-row gap-2 w-26 text-[#c13d4a] text-xl text-left hover:text-red-500 cursor-pointer transition"
+                >
+                    Log out
+                    <svg class="w-5 h-5 mt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2" />
+                    </svg>
+                </button>
+
+                {selectedFile && (
+                    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-green-600 px-6 py-3 rounded shadow-lg text-white z-50 animate-toast-slide-in">
+                        <button onClick={handleSave}>Save Changes</button>
+                    </div>
+                )}
+
+                {showResetPopup && (
+                    <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
+                        <div className="bg-[#1e1e1e] p-6 rounded-lg w-[90%] max-w-sm text-white shadow-xl">
+                            <h3 className="text-xl font-semibold mb-4">Reset Password</h3>
+                            <input
+                                type="email"
+                                value={resetEmail}
+                                onChange={(e) => setResetEmail(e.target.value)}
+                                placeholder="Enter your email"
+                                className="w-full mb-4 px-4 py-2 rounded bg-[#2a2a2a] text-white focus:outline-none"
+                            />
+                            <div className="flex justify-between">
+                                <button
+                                    className="bg-white font-bold px-4 py-1 rounded hover:bg-neutral-400 text-black transition hover:cursor-pointer"
+                                    onClick={handleReset}
+                                >
+                                    Send Link
+                                </button>
+                                <button
+                                    className="text-red-400 hover:underline hover:cursor-pointer"
+                                    onClick={() => {
+                                        setShowResetPopup(false);
+                                        setStatus('');
+                                    }}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                            {status && (
+                                <p className="mt-3 text-sm text-green-400">{status}</p>
+                            )}
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/pages/Profile/ProfilePicture.jsx
+++ src/pages/Profile/ProfilePicture.jsx
@@ -12,16 +12,19 @@ export default function ProfilePicture({ selectedFile, setSelectedFile, previewU
     };
 
     return (
-        <div className="relative group w-40 h-40 rounded-full overflow-hidden">
+        <div className="ml-3 border-10 border-[#0a0a0a] relative group w-43 h-43 rounded-lg overflow-hidden">
             <img
                 src={previewUrl || defaultPic}
                 alt="Profile"
-                className="object-cover w-full h-full"
+                className="object-cover w-full h-full rounded-md"
             />
 
             {/* Overlay */}
-            <label className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer text-sm text-white font-medium">
-                Edit Profile Picture
+            <label className="absolute inset-0 bg-black flex items-center justify-center opacity-0 group-hover:opacity-60 transition-opacity cursor-pointer text-sm text-white font-medium">
+                <svg class="w-9 h-9 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M14 4.182A4.136 4.136 0 0 1 16.9 3c1.087 0 2.13.425 2.899 1.182A4.01 4.01 0 0 1 21 7.037c0 1.068-.43 2.092-1.194 2.849L18.5 11.214l-5.8-5.71 1.287-1.31.012-.012Zm-2.717 2.763L6.186 12.13l2.175 2.141 5.063-5.218-2.141-2.108Zm-6.25 6.886-1.98 5.849a.992.992 0 0 0 .245 1.026 1.03 1.03 0 0 0 1.043.242L10.282 19l-5.25-5.168Zm6.954 4.01 5.096-5.186-2.218-2.183-5.063 5.218 2.185 2.15Z" clip-rule="evenodd" />
+                </svg>
+
                 <input
                     type="file"
                     accept="image/*"
