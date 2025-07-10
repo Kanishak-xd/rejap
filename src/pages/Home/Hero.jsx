d7c221a76d89b9a82dc6ef7dbe17ea57dabc0cf1
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-07-10 17:07:34 +0530

    added smooth scrolling and layout fixes to homepage

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,20 +1,25 @@
 export default function Hero() {
     return (
         <>
-            <section className=" h-screen flex justify-center">
-                <div className=" h-170 w-340 mt-15 flex justify-between items-center">
-                    <div className="h-140 w-200 flex justify-center items-center flex-col ml-20">
+            <section className="h-screen flex justify-center mt-10">
+                <div className="h-3/4 w-2/4 flex justify-between items-center">
+                    <div className="h-5/6 w-4/5 flex justify-center items-center flex-col pl-33">
                         <div className=" h-3/6 w-full flex flex-col items-start">
                             <p className="font-outfit font-bold text-left text-5xl/14  text-[#F3F3EE] cursor-default">Master Japanese — <br />One Lesson at a Time</p>
                             <p className="font-outfit font-regular text-4xl/11 text-[#F3F3EE] mt-4 cursor-default">Learn Japanese Scripts, time <br />expressions, and more through <br />fun quizzes & puzzles for free.</p>
                         </div>
                         <div className="h-1/6 w-full flex font-outfit text-3xl">
                             <a href="#scriptsSection">
-                                <button className="mt-4 text-[#F3F3EE] font-bold bg-[#141414] border-2 border-amber-50 h-15 w-70 rounded-xl cursor-pointer transform transition hover:-translate-y-1">START LESSON</button>
+                                <button className="mt-4 text-[#F3F3EE] font-bold bg-[#141414] border-2 border-amber-50 h-2/3 w-full px-6 rounded-xl cursor-pointer transform transition hover:scale-102">
+                                    GET STARTED
+                                </button>
                             </a>
                         </div>
                     </div>
-                    <div className="w-130 h-140 bg-[url('https://res.cloudinary.com/dykzzd9sy/image/upload/ramen-shop_caqgsz.png')] bg-contain bg-no-repeat bg-center animate-float"></div>
+                </div>
+                <div className="w-1/4 h-3/4 flex items-center">
+                    <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748898722/ramen-shop_caqgsz.png" className="h-3/4 w-3/4">
+                    </img>
                 </div>
             </section>
         </>
--- src/pages/Home/Scripts.jsx
+++ src/pages/Home/Scripts.jsx
@@ -1,28 +1,28 @@
-export default function Scripts(){
+export default function Scripts() {
     return (
         <>
             <section id="scriptsSection" className="h-screen flex justify-center">
-                <div className="h-[90%] w-[70%] mt-15 flex flex-col">
-                    <div className="w-full h-[90%] flex flex-col">
+                <div className="h-[90%] w-[70%] mt-10 flex flex-col">
+                    <div className="w-full h-[90%] flex flex-col items-center">
                         <div className="scriptsHeading w-full h-[15%] flex flex-col justify-center items-center">
                             <p className="font-outfit font-regular text-4xl text-amber-50 cursor-default">Japanese language uses</p>
                             <p className="font-outfit font-bold text-6xl text-amber-50 cursor-default">THREE SCRIPTS</p>
                         </div>
-                        <div className="scriptsFrames w-full h-[85%] flex flex-row justify-between items-center">
-                            <div className="hiraganaFrame w-110 h-full flex flex-col justify-between items-center">
-                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874095/card-1_hghjn7.webp" className="rounded-xl h-150 w-90"/>
+                        <div className="scriptsFrames w-9/10 h-[85%] mt-5 flex flex-row justify-between items-center">
+                            <div className="hiraganaFrame w-1/3 h-full flex flex-col justify-between items-center">
+                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874095/card-1_hghjn7.webp" className="rounded-md h-5/6 w-9/10" />
                                 <p className="font-outfit font-bold text-4xl text-amber-50 cursor-default">HIRAGANA</p>
-                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for native Japanese<br/>words</p>
+                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for native Japanese<br />words</p>
                             </div>
-                            <div className="katakanaFrame w-110 h-full flex flex-col justify-between items-center">
-                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874096/card-2_ucxqzl.webp" className="rounded-xl h-150 w-90"/>
+                            <div className="katakanaFrame w-1/3 h-full flex flex-col justify-between items-center">
+                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874096/card-2_ucxqzl.webp" className="rounded-md h-5/6 w-9/10" />
                                 <p className="font-outfit font-bold text-4xl text-amber-50 cursor-default">KATAKANA</p>
-                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for foreign<br/>words</p>
+                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for foreign<br />words</p>
                             </div>
-                            <div className="hiraganaFrame w-110 h-full flex flex-col justify-between items-center">
-                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874096/card-3_g9gaml.webp" className="rounded-xl h-150 w-90"/>
+                            <div className="hiraganaFrame w-1/3 h-full flex flex-col justify-between items-center">
+                                <img src="https://res.cloudinary.com/dykzzd9sy/image/upload/v1748874096/card-3_g9gaml.webp" className="rounded-md h-5/6 w-9/10" />
                                 <p className="font-outfit font-bold text-4xl text-amber-50 cursor-default">KANJI</p>
-                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for visual meaning<br/>and context</p>
+                                <p className="font-outfit font-regular text-3xl/8 text-amber-50 text-center cursor-default">used for visual meaning<br />and context</p>
                             </div>
                         </div>
                     </div>
@@ -30,7 +30,7 @@ export default function Scripts(){
                         <div className="flex flex-col justify-center items-center animate-float mt-12">
                             <a href="#chaptersSection" className="text-center flex justify-center items-center flex-col">
                                 <p className="font-bold text-amber-50 text-2xl mt-2">SCROLL DOWN</p>
-                                <svg className="-translate-y-3" xmlns="http://www.w3.org/2000/svg" width="48px" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z" stroke="#e3e3e3" stroke-width="30"/></svg>
+                                <svg className="-translate-y-3" xmlns="http://www.w3.org/2000/svg" width="48px" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z" stroke="#e3e3e3" stroke-width="30" /></svg>
                             </a>
                         </div>
                     </div>
