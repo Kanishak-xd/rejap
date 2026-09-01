4f580e087e166c629a4cdf9560239d741d7d7661
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2026-09-01 23:02:04 +0530

    fixed padding and margins on signin page and added background image to the gradient

--- frontend/src/pages/SignIn/LeftFrame/LeftFrame.jsx
+++ frontend/src/pages/SignIn/LeftFrame/LeftFrame.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function LeftFrame() {
     return (
-        <div className='purple-grad w-full h-full flex flex-col justify-end rounded-xl'>
+        <div className='bg-[url(https://res.cloudinary.com/dthdxxvhn/image/upload/v1788283577/sakura.avif)] bg-cover w-full h-full flex flex-col justify-end rounded-xl'>
             <div className='mb-8 ml-10 animate-float'>
-                <h2 className='text-[#F3F3EE] font-outfit font-bold text-6xl/10 cursor-default'>Learn Japanese</h2>
-                <h2 className='font-outfit font-regular text-[2.3rem] text-[#F3F3EE] mt-3 cursor-default'>Japanese Scripts,  Time expressions<br />Fun challenges & study resources</h2>
+                <h2 className='text-[#F3F3EE] font-outfit text-shadow-lg font-bold text-6xl/10 cursor-default'>Learn Japanese</h2>
+                <h2 className='font-outfit font-regular text-shadow-lg text-[2.3rem] text-[#F3F3EE] mt-3 cursor-default'>Japanese Scripts,  Time expressions<br />Fun challenges & study resources</h2>
             </div>
         </div>
     )
--- frontend/src/pages/SignIn/SignIn.jsx
+++ frontend/src/pages/SignIn/SignIn.jsx
@@ -4,14 +4,14 @@ import RightFrame from './rightFrame/RightFrame.jsx';
 
 export default function SignIn() {
     return (
-        <div className='w-full h-dvh flex flex-col sm:flex-row justify-center items-center gap-6 px-4 py-8 bg-neutral-950'>
+        <div className='w-full h-dvh flex flex-col sm:flex-row justify-center items-center gap-6 px-4 pt-10 pb-26 my-0 bg-neutral-950'>
             {/* LeftFrame */}
-            <div className="hidden xl:block xl:w-[54%] h-[88%] max-h-[800px]">
+            <div className="hidden xl:block xl:w-[54%] h-[100%] max-h-[1000px]">
                 <LeftFrame />
             </div>
 
             {/* RightFrame */}
-            <div className="w-full sm:w-[75%] md:w-[65%] lg:w-[50%] xl:w-[30%] max-w-xl h-[88%] max-h-[900px]">
+            <div className="w-full sm:w-[75%] md:w-[65%] lg:w-[50%] xl:w-[30%] max-w-xl h-[100%] max-h-[1000px]">
                 <RightFrame />
             </div>
         </div>
